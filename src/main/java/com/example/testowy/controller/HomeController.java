91fd2c066648c8cce7aef38b82a5626c64d5043f
lesni <rafal@dronology.pl>
2023-06-22 22:10:20 +0200

    dupa

--- src/main/java/com/example/testowy/controller/HomeController.java
+++ src/main/java/com/example/testowy/controller/HomeController.java
@@ -1,15 +1,26 @@
 package com.example.testowy.controller;
 
+import com.example.testowy.model.ModelName;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
 
 @Controller
 public class HomeController {
 
     @GetMapping("/")
     public String home(ModelMap map){
-        map.addAttribute("name", "home");
-        return "home";
+        map.addAttribute("modelName", new ModelName());
+        map.addAttribute("names", List.of("Kamil", "Marek", "Andrzej", "Robert"));
+        return "SelectNameEnter";
+    }
+    @PostMapping("/")
+    public String execute(ModelMap map, @ModelAttribute("modelName") ModelName name){
+        map.addAttribute("result", name.getName());
+        return "SelectNameExecute";
     }
 }
---
+++ src/main/java/com/example/testowy/model/ModelName.java
@@ -0,0 +1,12 @@
+package com.example.testowy.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ModelName {
+    private String name;
+}
