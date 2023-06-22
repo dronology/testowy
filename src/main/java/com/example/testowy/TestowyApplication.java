624d4d830dfdc4d3196bdcc69338a814cfd4ed82
lesni <rafal@dronology.pl>
2023-06-22 21:04:45 +0200

    dupa

---
+++ src/main/java/com/example/testowy/TestowyApplication.java
@@ -0,0 +1,13 @@
+package com.example.testowy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestowyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestowyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/testowy/TestowyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.testowy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestowyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
